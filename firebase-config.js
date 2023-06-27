ae64b496076aa33f6d5b401b1bfc61bced3adb50
ieuaingblg <113797276+ieuaingblg@users.noreply.github.com>
2023-06-27 19:17:05 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCCAhYEjsTyHIMZf0nDXEZZalcc8SYr5-M",
+    authDomain: "dramajepangmuwahid.firebaseapp.com",
+    databaseURL: "https://dramajepangmuwahid-default-rtdb.firebaseio.com",
+    projectId: "dramajepangmuwahid",
+    storageBucket: "dramajepangmuwahid.appspot.com",
+    messagingSenderId: "502760643649",
+    appId: "1:502760643649:web:f02b9b229802e113e83ac0",
+    measurementId: "G-G82VVWYPKX"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
